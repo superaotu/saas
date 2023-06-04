cc5ffb0d6193d731c4796e56d95f84fd1d529f64
刘兴 <liuxing997@foxmail.com>
2023-06-04 23:09:56 +0800

    Discard login API and add   group scores API.

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,46 +1,55 @@
 import request from "../utils/request";
 
 /**
- * 登录
- * @param {Object} param
- * @returns
+ * @description: 登录 （已废弃）
+ * @param {any} param
+ * @return {*}
  */
 export const login = (param: any) => {
   return request.post("/login", param);
 };
 
 /**
- * 获取课程信息
- * @param {Object} param
- * @returns
+ * @description: 获取课程信息
+ * @param {any} param
+ * @return {*}
  */
 export const getCourseList = (param: any) => {
   return request.get("/getAllCouse", param);
 };
 
 /**
- * 获取班级信息
- * @param {Object} param
- * @returns
+ * @description: 获取班级信息
+ * @param {any} param
+ * @return {*}
  */
 export const getClassList = (param: any) => {
   return request.get("/getAllClass", param);
 };
 
 /**
- * 获取所有活动信息
- * @param {Object} param
- * @returns
+ * @description: 获取所有活动信息
+ * @param {any} param
+ * @return {*}
  */
 export const getActiveList = (param: any) => {
   return request.post("/getActiveList", param);
 };
 
 /**
- * 获取学生所有分数
- * @param {Object} param
- * @returns
+ * @description:获取学生所有分数
+ * @param {any} param
+ * @return {*}
  */
 export const getScoreList = (param: any) => {
   return request.get("/getAllScore", param);
+};
+
+/**
+ * @description: 获取分组分数
+ * @param {any} param
+ * @return {*}
+ */
+export const getGroupScoreList = (param: any) => {
+  return request.get("/getGroupScore", param);
 };
